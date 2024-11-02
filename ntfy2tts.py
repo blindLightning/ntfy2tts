9ac48fa50fb6455d7b30c26bf3c951aea311aac7
blindlightning <blind.lightning.productions@gmail.com>
2024-11-02 12:51:14 +0000

    Adds a config option to use accessible output 3 as tts output.

--- ntfy2tts.py
+++ ntfy2tts.py
@@ -4,31 +4,41 @@ import signal
 import sys
 import ntfpy
 import pyttsx3
+from accessible_output3 import outputs as AO3
+
 
 #fix keyboard interrupts.
 signal.signal(signal.SIGINT, signal.SIG_DFL)
 
+
 async def main():
-	configFile=open("config.json")
-	config=json.load(configFile)
-	configFile.close()
-	url=config["url"]
-	topic=config["topic"]
-	client=ntfpy.NTFYClient(ntfpy.NTFYServer(url), topic)
-	await client.subscribe(speakMessage)
+    configFile=open("config.json")
+    config=json.load(configFile)
+    configFile.close()
+    url=config["url"]
+    topic=config["topic"]
+    screen_reader_output = False
+    speaker = None
+    screen_reader_output = config["screen_reader_output"]
+    if screen_reader_output: speaker = AO3.auto.Auto()
+    client=ntfpy.NTFYClient(ntfpy.NTFYServer(url), topic)
+    await client.subscribe(lambda message: speakMessage(message, screen_reader_output, speaker))
 
-def speak(text):
-	engin=pyttsx3.init()
-	engin.say(text)
-	engin.runAndWait()
+def speak(text, screen_reader_output: bool, speaker: any):
+    if not screen_reader_output:
+        engin=pyttsx3.init()
+        engin.say(text)
+        engin.runAndWait()
+    elif screen_reader_output and speaker is not None:
+        speaker.speak(text)
 
-def speakMessage(message: ntfpy.message.NTFYMessage):
-	starter=message.title if message.title is not None else message.topic
-	speak(f'{starter}: {message.message}')
+def speakMessage(message: ntfpy.message.NTFYMessage, screen_reader_output: bool, speaker: any):
+    starter=message.title if message.title is not None else message.topic
+    speak(f'{starter}: {message.message}', screen_reader_output, speaker)
 if __name__ == "__main__":
 
-	loop = asyncio.get_event_loop()
-	try:
-		loop.run_until_complete(main())
-	except asyncio.CancelledError:
-		pass
+    loop = asyncio.get_event_loop()
+    try:
+        loop.run_until_complete(main())
+    except asyncio.CancelledError:
+        pass
